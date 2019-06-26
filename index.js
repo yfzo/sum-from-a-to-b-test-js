d6e869bfa5294681bcc7460fb9f5da5c2539f2f0
Yifei <yifeizhou.to@gmail.com>
2019-06-26 04:48:29 +0000

    Completed exercise

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
 }
 
 module.exports = sum;
